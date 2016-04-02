9d1908441a4aaaaeb83926eb4c0625dccdb7517e
imxzjv <imxzjv@b4b63504-e89c-4449-b289-376832581794>
2016-04-02 03:10:38 +0000

    .21

--- AutoFollow.cs
+++ AutoFollow.cs
@@ -44,7 +44,7 @@ namespace AutoFollow
         }
 
         public static InterfaceLoader<IBehavior> Behaviors;
-        public static Version PluginVersion = new Version(1, 0, 20);
+        public static Version PluginVersion = new Version(1, 0, 21);
         internal static bool Enabled;
         internal static Message ServerMessage = new Message();
         internal static Dictionary<int, Message> ClientMessages = new Dictionary<int, Message>();
